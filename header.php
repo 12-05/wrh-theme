1b5b9b1fceda0971801b47c597cc5592b0378510
Magnus Westhofen <m.westhofen@12-05.de>
2024-02-15 14:54:25 +0100

    test

--- header.php
+++ header.php
@@ -1,32 +1,32 @@
 
 
-<?php 
-	if(get_field('title')) {
-		$title = get_field('title').' – '.get_bloginfo('name');
-	} else {
-		$title = get_the_title().' – '.get_bloginfo('name');
-	}
+<?php
+if (get_field('title')) {
+    $title = get_field('title') . ' – ' . get_bloginfo('name');
+} else {
+    $title = get_the_title() . ' – ' . get_bloginfo('name');
+}
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes();?> class="no-js">
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php echo $title;?></title>
+		<meta charset="<?php bloginfo('charset');?>">
+		<title><?php echo $title; ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/favicon.ico" rel="shortcut icon">
+		<link href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon.ico" rel="shortcut icon">
 		<link href="/wp-content/uploads/2021/05/richtershof-small.png" rel="apple-touch-icon-precomposed">
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name');?>" href="<?php bloginfo('rss2_url');?>" />
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php the_field('description') ?>" />
+		<meta name="description" content="<?php the_field('description')?>" />
 
-		<?php wp_head(); ?>
-	
+		<?php wp_head();?>
+
 
 	</head>
-	<body <?php body_class(); ?>>
+	<body <?php body_class();?>>
 		<style>
 			@media(max-width:700px) {
 	body > iframe {
@@ -43,44 +43,47 @@
 
 gs("GS00407",{ position : "center-left" });
 </script>
-		<?php if(is_front_page()) {?>
+		<?php if (is_front_page()) {?>
 			<iframe style="z-index:999999999;position:fixed;bottom:100px;right:0;border:0;width:300px;height:400px;" src="https://interfaces.cbooking.de/RateMonitor/index.html?client=9e5646ea-6ef6-489b-8c38-6ee03c9eca92&open=1&lang=de&showButton=1&update=1&buttonBgColor=a8925e&buttonColor=ffffff&headerBgColor=ffffff&headerColor=7f7f7f&contentColor=7f7f7f"></iframe>
 
-		<?php } ?>
-	
+		<?php }?>
+
 		<!-- wrapper -->
 		<div class="wrapper">
-      <?php $icons=get_field('iconsbar','option');$logo=get_field('hotellogo','option')?>
+      <?php $icons = get_field('iconsbar', 'option');
+$logo = get_field('hotellogo', 'option')?>
 			<!-- header -->
 			<header id='header' class="header clear top" role="banner">
           <div class='icon-bar'>
 			            <div class='icon-bar-small'>
-						  <?php if($icons) {
-                              foreach ($icons as $icon) {?>
-							  <a href='<?php echo $icon['link']['url']?>'><img src='<?php echo $icon['icon']?>'/></a>
+						  <?php if ($icons) {
+    foreach ($icons as $icon) {?>
+							  <a href='<?php echo $icon['link']['url'] ?>'><img src='<?php echo $icon['icon'] ?>'/></a>
 						  <?php }
-                        }?>
+}?>
 						</div>
 						<div class='icon-bar-big'>
-						<?php if($logo) {?>
-							  <a target='_blank' href='<?php echo get_field('hotellink','option')['url']?>'><img src='<?php echo $logo?>'/></a>
-						  <?php } ?>
+						<?php if ($logo) {?>
+							  <a target='_blank' href='<?php echo get_field('hotellink', 'option')['url'] ?>'><img src='<?php echo $logo ?>'/></a>
+						  <?php }?>
 						</div>
 					</div>
 					<a href="/"  class='icon'>
-                          <img src='<?php echo the_field('logo_small','option')?>'/>
+                          <img src='<?php echo the_field('logo_small', 'option') ?>'/>
 					</a>
 				  <div class='header-menu-bar' >
-						<a href='<?php echo get_field('kontaktlink','option')?>'>KONTAKT</a>
-						<a href='<?php echo get_field('buchen','option')?>'>BUCHEN</a>
+				  		<a href='/angebote'>ANGEBOTE</a>
+						<a href='/zimmer'>ZIMMER</a>
+						<a href='/zimmer'>FEIERN & TAGEN</a>
+						<a href='<?php echo get_field('buchen', 'option') ?>'>BUCHEN</a>
 						<div id="weglot_here"></div>
 						<div id='header-burger'></div>
 					</div>
-					
+
 			</header>
 			<div id="menu">
 			   <div id='header-burger-close'></div>
-         		<?php wp_nav_menu( array( 'menu' => 'whr-menu') ); ?>
+         		<?php wp_nav_menu(array('menu' => 'whr-menu'));?>
       </div>
 			<!-- /header -->
 
@@ -100,12 +103,12 @@ gs("GS00407",{ position : "center-left" });
    <div class='booking'>
       <div class='gast'>Gäste</div>
       <select name="gast" id="gast">
-        <?php for($i=1;$i<10;$i++){?>
-        <option <?php if($i == 2) {echo 'selected="selected"';}?> value="<?php echo $i?>"><?php echo $i?></option>
+        <?php for ($i = 1; $i < 10; $i++) {?>
+        <option <?php if ($i == 2) {echo 'selected="selected"';}?> value="<?php echo $i ?>"><?php echo $i ?></option>
         <?php }?>
      </select>
   </div>
-  <div class='button-group'> 
+  <div class='button-group'>
      <div class='whr-button' id='find-room'>ZIMMER FINDEN</div>
   </div>
 </div>
