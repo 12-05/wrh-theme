37ac7c487b3a48fbfa1d44c28e019b1ca6d0a343
Magnus Westhofen <m.westhofen@12-05.de>
2024-02-23 11:44:59 +0100

    test

--- whr/inhalt/fulltext.php
+++ whr/inhalt/fulltext.php
@@ -1,7 +1,7 @@
 <div class='row fulltext'>
-  <h1><?php echo $block['headline']?></h1>
-  <p><?php echo $block['text']?></p>
-  <?php if($block['link']){?>
-  <a class='link' href='<?php echo $block['link']['url']?>'><?php echo $block['link']['title']?></a>
+  <h1><?php echo $block['headline'] ?></h1>
+  <p><?php echo do_shortcode($block['text']); ?></p>
+  <?php if ($block['link']) {?>
+  <a class='link' href='<?php echo $block['link']['url'] ?>'><?php echo $block['link']['title'] ?></a>
   <?php }?>
 </div>
